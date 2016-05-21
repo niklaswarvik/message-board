a5cc917a0c77b85ffa81312c626cf59e6a160b72
Niklas Wärvik <niklas.warvik@gmail.com>
2016-05-21 21:41:34 +0200

    serve html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
@@ -11,3 +12,7 @@ app.use(bodyParser.json());
 var routes = require('./apiRoutes')(app, express);
 app.use('/api', routes);
 app.listen(3000);
+
+app.get('/',(err, res) => {
+	res.sendFile(path.join(__dirname, '/static', 'index.html'));
+});
